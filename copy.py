5ab32de6a4b3f777aa3326d483bc8fbb95ccced0
Nikolay Mitchenko <fireman777@ukr.net>
2019-01-08 17:40:23 +0200

    copy.py done v.1.0

--- copy.py
+++ copy.py
@@ -1,7 +1,20 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python36
 
-import os 
-print('Hello World!)
-source='/home/docker'
-target='/home/docker'
+#version 1.0
+import os
+import time
+
+#vars
+source='/home/docker/aws'
+target='/home/docker/backup/'+time.strftime('%d%m%y_%H%M')
+copy_command='cp -r '+source+' '+target
+
+if not os.path.exists(target):
+    os.makedirs(target)
+
+if os.system(copy_command)==0:
+    print('Copied to '+target)
+else:
+    print('Error!')
+
 
