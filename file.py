3a79cb58e7aba5283a62f7b3145583433f83f2c6
Nikolay Mitchenko <fireman777@ukr.net>
2019-01-09 10:28:40 +0200

    file.py done

--- file.py
+++ file.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python36
 
-print("Hello, World!")
+#open file1.txt and copy each symbol to file3.txt
 
+myfile = open("file1.txt", "r")
+cont=myfile.read()
+
+myfile3 = open("file3.txt", "w")
+for i in cont:
+    myfile3.write(i)
+
+myfile.close()
+myfile3.close()
