23de8f20585d83ca123fe1bbe933b388c864a605
fireman777 <39376634+fireman777@users.noreply.github.com>
2019-01-10 19:31:02 +0200

    Create classes.py

---
+++ classes.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+#version 2.0-make .zip archieve of repo
+
+class numb:
+    def __init__(self,number_1,number_2):
+      self.number_1=number_1
+      self.number_2=number_2
+    
+    def mnoz(self,number_1,number_2):
+        print(number_1*number_2)
+        
+    def delen(self,number_1,number_2):
+        print(number_1/number_2)
+
+    def plus(self,number_1,number_2):
+        print(number_1+number_2)
+    
+    def minus(self,number_1,number_2):
+        print(number_1-number_2)
+    
+        
