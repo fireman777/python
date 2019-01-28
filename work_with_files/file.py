ce964053e141cbd99bea5a12cef1169042821d00
Nikolay Mitchenko <fireman777@ukr.net>
2019-01-28 16:33:42 +0200

    work with files

--- work_with_files/file.py
+++ work_with_files/file.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python36
 
-#open file1.txt and push all test to variable
-myfile1 = open("file1.txt", "r")
-content = myfile1.read()
-myfile1.close()
+#open and close file using try/finally and push all content to variable
+try:
+    f = open("file1.txt")
+    content=f.read()
+finally:
+    f.close()
 
 #open file3.txt and pull there all the symbols from myfile1.txt
 myfile3 = open("file3.txt", "w")
@@ -13,13 +15,6 @@ for i in content:
 #add some bytes (symbols) and print their number
 print(myfile3.write('abcdef'))
 myfile3.close()
-
-#print some bytes(symbols) of file1.txt and close it using try and finally
-try:
-    f = open("file1.txt")
-    print(f.read(19))
-finally:
-    f.close()
 
 #print some bytes(symbols) of and close it using with statement
 with open("file1.txt") as f:
