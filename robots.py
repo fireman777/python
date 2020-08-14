3c48556f606350d0dd22024efe3e2b654456d557
Mykola Mitchenko <mykola_mitchenko@epam.com>
2020-08-14 11:38:00 +0300

    robots.py updated

--- robots.py
+++ robots.py
@@ -2,19 +2,31 @@
 
 class Robot:
     '''Представляет робота с именем.'''
+    #Переменная класса, содержащая количество роботов
     population=0
 
-    def __init__(self, name):
-        self.name=name
-        print()
+    def __init__(self,name):
+        '''Инициализация данных.'''
+        self.name  name
         print('Инициализация {0}.'.format(self.name))
-        Robot.population+=1
+        '''Инициализация данных.'''
 
+        Robot.population += 1
+
+    def __del__(self):
+        '''Я умираю.'''
+        print('{} уничтожается!'.format(self.name))
+        Robot.population -= 1
+        if Robot.population == 0:
+            print('{} был последним.'.format(self.name))
+        else:
+            print('Осталось {0:d} работающих роботов.'.format(Robot.population))
     def sayhi(self):
         print('Привет, меня зовут {0}.'.format(self.name))
 
     def howmany():
         print('у нас {0:d} роботов.'.format(Robot.population))
+
     howmany=staticmethod(howmany)
 
 droid1=Robot('Vasya')
@@ -28,5 +40,3 @@ Robot.howmany()
 droid3=Robot('Grisha')
 droid3.sayhi()
 Robot.howmany()
-
-
